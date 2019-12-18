eb2b881cf871c849e64886f62457bb5c14d713d0
USUARIO 03 <USUARIO 03@DESKTOP-2BVO22Q>
2019-12-17 21:45:27 -0500

    Boton que cambia

---
+++ src/practican/usaPracticaN2.java
@@ -0,0 +1,12 @@
+
+package practican;
+
+
+public class usaPracticaN2 {
+    public static void main(String[] args) {
+        PracticaN2 c = new PracticaN2();
+        c.setVisible(true);
+        c.setLocationRelativeTo(null);
+    }
+  
+}
